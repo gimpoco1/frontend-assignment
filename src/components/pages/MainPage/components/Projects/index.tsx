94a13616e72d9b41c3c04dc45d22ce8f2612f9a5
Giacomo <impoco126@gmail.com>
2024-10-04 18:16:06 +0200

    added animation for when sorting

--- src/components/pages/MainPage/components/Projects/index.tsx
+++ src/components/pages/MainPage/components/Projects/index.tsx
@@ -1,6 +1,9 @@
 import { Grid } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 import ProjectCard from "../../../../organisms/ProjectCard";
 import { Project } from "../../../../../data/types";
+
+const MotionBox = motion.div;
 
 function Projects({
   projects,
@@ -12,11 +15,16 @@ function Projects({
   return (
     <Grid templateColumns="repeat(auto-fill, minmax(250px, 1fr))" gap={6}>
       {projects.map((project) => (
-        <ProjectCard
+        <MotionBox
           key={project.id}
-          project={project}
-          onRemove={removeProject}
-        />
+          layout
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 0.9 }}
+          transition={{ duration: 0.5 }}
+        >
+          <ProjectCard project={project} onRemove={removeProject} />
+        </MotionBox>
       ))}
     </Grid>
   );
